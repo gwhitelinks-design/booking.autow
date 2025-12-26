8acf22d65769b6083296e50975f73eb51b870fbe
D Menet <linkensrt8@gmail.com>
2025-12-26 19:25:34 +0000

    Debug environment variables

--- app/api/test-env/route.ts
+++ app/api/test-env/route.ts
@@ -3,12 +3,18 @@ import { NextResponse } from 'next/server';
 export const dynamic = 'force-dynamic';
 
 export async function GET() {
+  const username = process.env.AUTOW_STAFF_USERNAME;
+  const password = process.env.AUTOW_STAFF_PASSWORD;
+
   return NextResponse.json({
-    hasUsername: !!process.env.AUTOW_STAFF_USERNAME,
-    hasPassword: !!process.env.AUTOW_STAFF_PASSWORD,
+    hasUsername: !!username,
+    hasPassword: !!password,
     hasToken: !!process.env.AUTOW_STAFF_TOKEN,
-    usernameLength: process.env.AUTOW_STAFF_USERNAME?.length || 0,
-    passwordLength: process.env.AUTOW_STAFF_PASSWORD?.length || 0,
+    usernameValue: username, // Show actual value to debug
+    usernameLength: username?.length || 0,
+    passwordLength: password?.length || 0,
+    passwordFirst3: password?.substring(0, 3), // First 3 chars to verify
     tokenLength: process.env.AUTOW_STAFF_TOKEN?.length || 0,
+    allEnvKeys: Object.keys(process.env).filter(k => k.startsWith('AUTOW')),
   });
 }
